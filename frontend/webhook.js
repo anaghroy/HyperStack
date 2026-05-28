b839182612eff980306677584ed69977dd877a0f
AnaghRoy <royanagh1999@gmail.com>
2026-05-28 12:55:20 +0530

    Graph Implemantation

--- frontend/webhook.js
+++
@@ -1,2 +0,0 @@
-//https://discord.com/api/webhooks/1508707098761101382/1HpeZPCYx551ZIrh43bU1bISMuMu2fVxXk7W0djkYGWOYbb9mlKuVo_S7kV5T87aPpHa
-// https://hooks.slack.com/services/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
